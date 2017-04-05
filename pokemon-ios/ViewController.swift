2e8eb935742fcd5d5e24a9f702a25325de69441b
Matias Elorriaga <matoelorriaga@gmail.com>
2017-04-04 22:16:50 -0300

    Initial Commit

---
+++ pokemon-ios/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  pokemon-ios
+//
+//  Created by Matías Elorriaga on 4/4/17.
+//  Copyright © 2017 melorriaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
